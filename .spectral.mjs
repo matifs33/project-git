c5d9f13cb53ecbf143b9df8b7278df15226cc05e
ferrerasmatias@outlook.com <ferrerasmatias@outlook.com>
2024-04-10 14:32:02 +0200

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0Mjc4Nw/spectral.js?branch_slug=master&token=bf618cb9-e994-49e3-8a49-95d5e56096a2";
+export default { extends: ruleset };
